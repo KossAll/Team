7acc5de9570aa6d1e40f3b9e63380bca3a5d0809
KossAll <108170291+KossAll@users.noreply.github.com>
2023-09-06 17:30:17 +0000

    Deploying to gh-pages from @ KossAll/Team@93713966a8bf3b336442d7b578e104f8310af2e9 🚀

---
+++ index.1f154b39.js
@@ -0,0 +1,2 @@
+!function(){var e={openModal2Btn:document.querySelector("[data-modal2-open]"),closeModal2Btn:document.querySelector("[data-modal2-close]"),modal2:document.querySelector("[data-modal2]")};function o(){e.modal2.classList.toggle("is-hidden")}e.openModal2Btn.addEventListener("click",o),e.closeModal2Btn.addEventListener("click",o)}();
+//# sourceMappingURL=index.1f154b39.js.map
