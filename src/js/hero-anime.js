df82684eed53d3e1288506ad3c94365ab152e21f
Alexandr-Diachenko <wildfow85@gmail.com>
2023-10-01 15:10:10 +0300

    hero

---
+++ src/js/hero-anime.js
@@ -0,0 +1,38 @@
+// document.addEventListener('DOMContentLoaded', () => {
+//     let currentIndex = 0;
+// const images = [
+//   '../images/IMG_1595.jpeg',
+//   '../images/IMG_1320.jpeg'
+// ];
+
+// function changeBackground() {
+//   const heroElement = document.querySelector('.hero');
+//   heroElement.style.backgroundImage = `linear-gradient(0deg, rgba(0, 0, 0, 0.4), rgba(0, 0, 0, 0.4)), url(${images[currentIndex]})`;
+//   currentIndex = (currentIndex + 1) % images.length;
+// }
+
+// setInterval(changeBackground, 15000);
+
+//   });
+  
+
+
+// const images = [
+//     'url(../images/IMG_1595.jpeg)',
+//     'url(../images/IMG_1320.jpeg)',
+//     // Add more image URLs as needed
+// ];
+
+// let currentIndex = 0;
+// const heroElement = document.querySelector('.hero');
+
+// function changeImage() {
+//     heroElement.style.backgroundImage = images[currentIndex];
+//     currentIndex = (currentIndex + 1) % images.length;
+// }
+
+// setInterval(changeImage, 15000); // Change image every 15 seconds
+
+
+
+
